4460a433f0d228261e6c1a0cf4d1e8b3744dcd37
Abhishek Mishra <abhishek.mishra@w3villa.com>
2022-01-17 18:51:36 +0530

    Added price update and total price feature

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,48 @@
 class ItemsController < ApplicationController
 
-	def index
+	before_action :define, only: [:total, :calculate_price]
+
+	def home
 	end
 
 	def list
 		@items = Item.all
 	end
+
+	def edit
+		@item = Item.find(params[:id])
+	end
+
+	def update
+		@item = Item.find(params[:id])
+		if @item.update(params.require(:item).permit(:price))
+			redirect_to items_path	
+			flash[:notice] = "Price updated successfully"
+		else
+			render 'edit'
+		end
+	end
+
+	def price
+	end
+
+	def total
+	end
+
+	def calculate_price
+		$total_price = ((params[:quantity][:MUG].to_i*@mug_price) + 
+						(params[:quantity][:TSHIRT].to_i*@tshirt_price) +
+						(params[:quantity][:HOODIE].to_i*@hoodie_price))
+		redirect_to total_path
+		flash[:notice] = "price calculated successfully..."
+	end
+
+	private
+
+		def define
+			@mug_price = Item.find_by(code: "MUG").price
+			@tshirt_price = Item.find_by(code: "TSHIRT").price
+			@hoodie_price = Item.find_by(code: "HOODIE").price
+		end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'items#index'
-  get 'list', to: 'items#list'
+  root to: 'items#home'
+  get 'items', to: 'items#list'
+  get 'price', to: 'items#price'
+  post 'price', to: 'items#calculate_price'
+  get 'total', to: 'items#total'
+  resources :items, only: [:edit, :update]
 
 end
